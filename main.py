bbc0f4de7d3f11ea747be8e460b900a1184439b3
Hansel Lee <mrhansellee@gmail.com>
2020-05-16 23:38:23 -0700

    Moved to a class based model

--- main.py
+++ main.py
@@ -13,17 +13,8 @@ from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ExpectedConditions
 
-# We need to input a zip code for this to work
-chrome_options = Options()
-# chrome_options.add_argument('--headless')
-chrome_options.add_argument('log-level=3')
-driver = webdriver.Chrome(chrome_options=chrome_options)
-LINK = 'https://sayweee.com/en'
-PRODUCT_BASE_LINK = 'https://www.sayweee.com/product/view/'
 with open('secrets.txt', 'r') as file:
-    SECRETS = file.readlines()
-
-ZIP = SECRETS[0]
+        SECRETS = file.readlines()
 
 class Item():
     """Basic class to store information of not sold out items"""
@@ -34,136 +25,143 @@ class Item():
         self.name = name
         self.id = num
 
-def click_out():
-    """Simple function to click out of modal"""
-    webdriver.ActionChains(driver).send_keys(Keys.ESCAPE).perform()
+class Scraper():
+    """Represents scraper process, class is used because it allows threads to access common constants"""
+    def __init__(self):
+        """instantiating webdriver instance and defining constants"""
+        chrome_path = r'..\chromedriver.exe'
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('log-level=3')
+        self.driver = webdriver.Chrome(executable_path=chrome_path, chrome_options=chrome_options)
 
-def sub_scrape(already_found, previously_found):
-    """scraping individual tabs"""
-    products = driver.find_elements_by_class_name('product-media')
-    for product in products:
-        try:
-            tag = product.find_element_by_css_selector('div.product-unavailable')
+        self.LINK_TEXTS = ['Fruit', 'Greens', 'Meat', 'Seafood', 'Restaurant',
+                'Dim Sum', 'Ready-Made', 'Bakery', 'Snacks', 'Dairy',
+                'Dried Goods', 'Seasonings', 'Personal Care', 'Groceries']
+        self.food_found = set()
+        self.LINK = 'https://sayweee.com/en'
+        self.PRODUCT_BASE_LINK = 'https://www.sayweee.com/product/view/'
 
-        except NoSuchElementException:    
-            price = f"${product.get_attribute('data-product-price')}"
-            image_link = product.find_element_by_class_name('lazy-b').get_attribute('src')
-            name = product.find_element_by_class_name('product-title').text.strip()
-            product_id = product.get_attribute('data-product-id')
-            link = PRODUCT_BASE_LINK + product_id
-            if name == '':
-                pass
+    def click_out(self):
+        """Simple function to click out of ad"""
+        webdriver.ActionChains(self.driver).send_keys(Keys.ESCAPE).perform()
 
-            else:
-                if product_id in already_found:
+    def sub_scrape(self, already_found, previously_found):
+        """scraping individual tabs of category"""
+        products = self.driver.find_elements_by_class_name('product-media')
+        for product in products:
+            try:
+                _ = product.find_element_by_css_selector('div.product-unavailable')
+
+            except NoSuchElementException:    
+                price = f"${product.get_attribute('data-product-price')}"
+                image_link = product.find_element_by_class_name('lazy-b').get_attribute('src')
+                name = product.find_element_by_class_name('product-title').text.strip()
+                product_id = product.get_attribute('data-product-id')
+                link = self.PRODUCT_BASE_LINK + product_id
+                if name == '':
                     pass
 
                 else:
-                    if product_id not in previously_found:
-                        product = Item(price, image_link, name, link, product_id)
-                        food_found.add(product)
-                        already_found.append(product_id)
-                        print(product.name)
+                    if product_id in already_found:
+                        pass
 
-                    else: pass
+                    else:
+                        if product_id not in previously_found:
+                            product = Item(price, image_link, name, link, product_id)
+                            self.food_found.add(product)
+                            already_found.append(product_id)
+                            print(product.name)
 
-    return already_found
+                        else: pass
 
-def record():
-    """Records found food into previously_found.txt"""
-    with open('previously_found.txt', 'w') as file:
-        for product in food_found:
-            file.write(f'{product.id}\n')
+        return already_found
 
-def scrape_page():
-    """Used to find non-sold out food
+    def record(self):
+        """Records found food into previously_found.txt"""
+        with open('previously_found.txt', 'w') as file:
+            for product in self.food_found:
+                file.write(f'{product.id}\n')
 
-    1. Iterates through filters
-    2. Finds any food that isn't sold out
-    """
-    prev = []
-    with open('previously_found.txt', 'r') as file:
-        ids = file.readlines()
-        for i in ids:
-            i = i.strip('\n')
-            prev.append(i)
+    def scrape_page(self):
+        """Used to find non-sold out food
 
-    already_found = sub_scrape([], prev)
-    try:
-        filter_list = driver.find_element_by_class_name('list-filter')
-        filters = filter_list.find_elements_by_css_selector('.filter-item')
+        1. Iterates through filters
+        2. Finds any food that isn't sold out
+        """
+        prev = []
+        with open('previously_found.txt', 'r') as file:
+            ids = file.readlines()
+            for i in ids:
+                i = i.strip('\n')
+                prev.append(i)
 
-        back_times = len(filters)
-        for filter in filters:                 
-            filter.click()
-            already_found = sub_scrape(already_found, prev)
-
-        return back_times
-
-    except:
-        return 1
-        
-driver.get(LINK)
-input = driver.find_element_by_id('zip_code')
-input.send_keys(ZIP)
-submit_button = driver.find_element_by_id('save-btn')
-
-# Wait for page to load
-time.sleep(0.2)
-
-# Click out of pop out ad 
-click_out()
-
-LINK_TEXTS = ['Fruit', 'Greens', 'Meat', 'Seafood', 'Restaurant',
-            'Dim Sum', 'Ready-Made', 'Bakery', 'Snacks', 'Dairy',
-            'Dried Goods', 'Seasonings', 'Personal Care', 'Groceries']
-food_found = set()
-
-# Clicks on each predefined hyperlink
-for link in tqdm(LINK_TEXTS):
-    trying = True
-    while trying:
+        already_found = self.sub_scrape([], prev)
         try:
-            trying = False
-            page = driver.find_element_by_link_text(link)
-            page.click()
-            back_times = scrape_page()
-            back_button = driver.find_element_by_class_name('back')
-            for i in range(back_times):
-                back_button.click()
+            filter_list = self.driver.find_element_by_class_name('list-filter')
+            filters = filter_list.find_elements_by_css_selector('.filter-item')
 
-            time.sleep(0.7)
-            click_out()
+            for filt in filters:                 
+                filt.click()
+                already_found = self.sub_scrape(already_found, prev)
 
-        except:
-            click_out()
+        finally:
+            return
 
-record()
+    def main(self, link): 
+        """Don't call me, call main_wrapper"""
+        ZIP = SECRETS[0]
+        self.driver.get(self.LINK)
+        input = self.driver.find_element_by_id('zip_code')
+        input.send_keys(ZIP)
+        _ = self.driver.find_element_by_id('save-btn')
 
-# Sends email notifying you of available products
-def send(products):
-    if len(products) != 0:
-        text = []
-        for item in products:
-            text.append(f'{item.name}\n{item.price}\n{item.link}')
+        # Click out of pop out ad 
+        self.click_out()
+        time.sleep(0.5)
 
-        final_text = "\n\n".join(text)
-        EMAIL_ADDRESS = SECRETS[1]
-        EMAIL_PASSWORD = SECRETS[2]
-        TARGET = SECRETS[3]
-        message = MIMEMultipart('alternative')
-        message['Subject'] = 'SayWeee Update'
-        message['From'] = EMAIL_ADDRESS
-        message['To'] = TARGET
+        trying = True
+        while trying:
+            try:
+                page = self.driver.find_element_by_link_text(link)
+                page.click()
+                self.scrape_page()
+                return
 
-        part1 = MIMEText(final_text, 'plain')
-        message.attach(part1)
+            except:
+                trying = True
+                self.click_out()
 
-        with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-            smtp.send_message(message)
+    # Sends email notifying you of available products
+    def send(self, products):
+        if len(products) != 0:
+            text = []
+            for item in products:
+                text.append(f'{item.name}\n{item.price}\n{item.link}')
 
-    else:
-        print('Didnt send')
+            final_text = "\n\n".join(text)
+            EMAIL_ADDRESS = SECRETS[1]
+            EMAIL_PASSWORD = SECRETS[2]
+            TARGET = SECRETS[3]
+            message = MIMEMultipart('alternative')
+            message['Subject'] = 'SayWeee Update'
+            message['From'] = EMAIL_ADDRESS
+            message['To'] = TARGET
 
-send(food_found)
+            part1 = MIMEText(final_text, 'plain')
+            message.attach(part1)
+
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(message)
+
+        else:
+            print('Didnt send')
+
+    def main_wrapper(self):
+        """call this"""
+        for link in self.LINK_TEXTS:
+            self.main(link)
+
+        self.record()
+        self.send(self.food_found)
